57ad28c63ff01b309e2db83022a566ab4c93531b
Nick Dalzell <dalzelln@gmatc.matc.edu>
2022-10-31 17:57:23 -0500

    almost finished

--- server.js
+++ server.js
@@ -72,21 +72,7 @@ app.post(
                 await user.save();
 
                 //generate and return a jwt token
-                const payload = {
-                    user: {
-                        id: user.id
-                    }
-                };
-
-                jwt.sign(
-                    payload,
-                    config.get('jwtSecret'),
-                    { expiresIn: '10hr'},
-                    (err, token) => {
-                        if(err) throw err;
-                        res.json({ token: token});
-                    }
-                );                
+                returnToken(user, res);               
             } catch (error) {
                 res.status(500).send('Server error');
             }
