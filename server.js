9985e47f52fc05a166af2403c79d0a52a593d294
Nick Dalzell <dalzelln@gmatc.matc.edu>
2022-10-31 19:06:23 -0500

    started activity 9

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import jwt from 'jsonwebtoken';
 import config from 'config';
 import User from './models/User';
 import auth from './middleware/auth';
+import Post from './models/Post';
 
 //init express app
 const app = express();
@@ -152,6 +153,52 @@ const returnToken = (user, res) => {
         }
     );      
 };
+
+/**
+ * @route POST api/post
+ * @desc Create post
+ */
+app.post(
+    '/api/posts',
+    [
+        auth,
+        [
+            check('title', 'Title text is required')
+            .not()
+            .isEmpty(),
+            check('body', 'Body text is required')
+            .not()
+            .isEmpty()
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            res.status(400).json({ errors: errors.array() });
+        } else {
+            const { title, body} = req.body;
+            try {
+                //get the user who created the post
+                const user = await User.findById(req.user.id);
+
+                //create new post
+                const post = new Post({
+                    user: user.id,
+                    title: title,
+                    body: body
+                });
+
+                //save to the db and return
+                await post.save();
+
+                res.json(post);
+            } catch (error) {
+                console.error(error);
+                res.status(500).send('Server error');
+            }
+        }
+    }
+);
 
 
 //connection listener
